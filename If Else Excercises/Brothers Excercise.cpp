a0f98d332c0c4f2ab94bf83e477b051ed904329d
AndrheyL <91404735+AndrheyL@users.noreply.github.com>
2021-11-16 10:22:51 +0400

    Add files via upload

---
+++ If Else Excercises/Brothers Excercise.cpp
@@ -0,0 +1,15 @@
+// Brothers Excercise.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int brotherA = 15;
+    int brotherB = 12;
+
+    if (!(brotherA < brotherB)) {
+        std::cout << "Brother A is Older!";
+    }
+}
+
---
+++ If Else Excercises/Porridge Excercise.cpp
@@ -0,0 +1,14 @@
+// Porridge Excercise.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int porridgeTemperature = 56;
+    if ((porridgeTemperature > 40) && (porridgeTemperature < 60))
+    {
+    std::cout << "That's just right" << std::endl;
+    }
+}
+
