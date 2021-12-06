cb678a8919abf231a81a2031b3fbcc4e08320fba
AndrheyL <91404735+AndrheyL@users.noreply.github.com>
2021-12-06 22:17:22 +0400

    Add files via upload

---
+++ Lecture 12.cpp
@@ -0,0 +1,192 @@
+// Lecture 12.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+//int main()
+//{
+//	string UWU[4][5] =
+//	{
+//		{"_", " _", "_", "_ ", "_"},
+//		{"| ", "U", " ", "U", " |"},
+//		{"| ", " ", "W", " "," |"},
+//		{"-", " -", "-", "- ", "-"}
+//	};
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = 0; j < 5; j++)
+//		{
+//			cout << UWU[i][j];
+//		
+//		}
+//		cout << endl;
+//	}
+//}
+
+//int main()
+//{
+//	string snacks[3][4] =
+//	{
+//		{"Galaxy Silk", "Mars Bar", "Snickers", "Bounty"},
+//		{"Flavoured Youghurt", "Oman Chips", "Oreo", "Lays"},
+//		{"Apple", "Banana", "Orange", "Pear"}
+//	};
+//	for (int i = 0; i < 3; i++)
+//	{
+//		for (int j = 0; j < 4; j++)
+//		{
+//			cout << snacks[i][j] << " | ";
+//
+//		}
+//		cout << endl;
+//	}
+//}
+
+//int main()
+//{
+//	int integers[5];
+//	cout << "Enter numbers" << endl;
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cin >> integers[i];
+//		
+//	}
+//	cout << "Here are the numbers you inputted" << endl;
+//	for (int j = 0; j < 5; j++)
+//	{
+//		cout << integers[j] << " ";
+//	}
+//	
+//}
+
+//int main()
+//{
+//	char letters[] = { 'C', 'o', 'd', 'e', 'L', 'a', 'b' };
+//	for (auto letter : letters)
+//	{
+//		cout << letter;
+//	}
+//}
+
+//int main()
+//{
+//	string courses[] = { "BSU CC", "BSU BA", "HNC CC", "HND" };
+//
+//	for (int i = 0; i < 4; i++)
+//	{
+//		cout << courses[i] << endl << endl;
+//	}
+//	for (auto course : courses)
+//	{
+//		cout << course << endl;
+//	}
+//}
+
+//int main()
+//{
+//  ERROR ERROR
+//	int marks[5];
+//	int sum = 0;
+//	for (int i = 1; i < 6; i++)
+//	{
+//		cout << "Enter marks" << endl;
+//		cin >> marks[i];
+//	}
+//	cout << "This is the number values you entered for the marks" << endl;
+//	for (int j = 1; j < 6; j++)
+//	{
+//		cout << marks[j] << endl;
+//	}
+//}
+
+//int main()
+//{
+//    TEST
+//    int n, i;
+//    float num[100], sum = 0.0, average;
+//
+//    cout << "Enter the numbers of data: ";
+//    cin >> n;
+//
+//    while (n > 100 || n <= 0)
+//    {
+//        cout << "Error! number should in range of (1 to 100)." << endl;
+//        cout << "Enter the number again: ";
+//        cin >> n;
+//    }
+//
+//    for (i = 0; i < n; ++i)
+//    {
+//        cout << i + 1 << ". Enter number: ";
+//        cin >> num[i];
+//        sum += num[i];
+//    }
+//
+//    average = sum / n;
+//    cout << "Average = " << average;
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//	string array[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+//
+//	for (string i = 0; i < 12; i++)
+//	{
+//		cout << array[i] << endl;
+//	}
+//}
+
+//int main()
+//{
+//	int heartRates[8] = { 54, 60, 71, 59, 62, 63, 60, 58 };
+//	
+//	for (int i = 0; i < 8; i++)
+//	{
+//		cout << heartRates[i] << endl;
+//	}
+//}
+
+//int main()
+//{
+//	int heartRates[8] = { 54, 60, 71, 59, 62, 63, 60, 58 };
+//
+//	cout << heartRates[3] << endl;
+//	cout << heartRates[6] << endl;
+//}
+
+//int main()
+//{
+//	string month[12];
+//	month[1] = "January";
+//	month[2] = "February";
+//	month[3] = "March";
+//	month[4] = "April";
+//	month[5] = "May";
+//	month[6] = "June";
+//	month[7] = "July";
+//	month[8] = "August";
+//	month[9] = "September";
+//	month[10] = "October";
+//	month[11] = "November";
+//	month[12] = "December";
+//
+//	string array[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+//}
+
+
+
+
+//int main()
+//{
+//    int ages[5];
+//    ages[0] = 19;
+//    ages[1] = 23;
+//    ages[2] = 22;
+//    ages[3] = 30;
+//    ages[4] = 18;
+//
+//    int array[5] = {19, 23, 22, 30, 18};
+//}
